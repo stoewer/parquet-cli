7ba6055c5dc0894379efc1939676a7ef315bfd81
A. Stoewer <adrian@stoewer.me>
2022-12-09 11:16:21 +1000

    Sub command to show column sttistics

--- cmd/parquet-cli/cmd_col_stats.go
+++ cmd/parquet-cli/cmd_col_stats.go
@@ -1,13 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/stoewer/parquet-cli/pkg/inspect"
+	"github.com/stoewer/parquet-cli/pkg/output"
+)
 
 type colStats struct {
 	outputOptions
-	File string `arg:""`
+	File    string `arg:""`
+	Columns []int  `short:"c" optional:"" help:"Restrict the output to the following columns"`
 }
 
 func (cs *colStats) Run() error {
-	fmt.Println("Sub command col-stats not implemented yet")
-	return nil
+	file, err := openParquetFile(cs.File)
+	if err != nil {
+		return err
+	}
+
+	rowStats, err := inspect.NewColStatCalculator(file, cs.Columns)
+	if err != nil {
+		return err
+	}
+
+	return output.Print(os.Stdout, cs.Output, rowStats)
 }
