9209325ca3009df4759f9c8b89387ffc5ce5bd10
A. Stoewer <adrian@stoewer.me>
2022-12-09 18:17:55 +1000

    Sub command to print the version

--- cmd/parquet-cli/main.go
+++ cmd/parquet-cli/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/alecthomas/kong"
 	"github.com/stoewer/parquet-cli/pkg/output"
 )
 
+var (
+	Version = "n.a."
+	Commit  = "n.a."
+)
+
 var cli struct {
+	Version  version  `cmd:"" help:"Print the parquet-cli version"`
 	Schema   schema   `cmd:"" help:"Print the files schema"`
 	ColStats colStats `cmd:"" help:"Show column numbers and statistics from a file"`
 	RowStats rowStats `cmd:"" help:"Show statistics about each row in a file"`
@@ -27,6 +35,14 @@ func main() {
 		kong.Description("A tool to analyze the schema and content of parquet files"),
 		kong.UsageOnError(),
 	)
+
 	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
+}
+
+type version struct{}
+
+func (v version) Run() error {
+	fmt.Printf("parquet-cli: version=%s commit=%s\n", Version, Commit)
+	return nil
 }
